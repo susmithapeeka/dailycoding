fcae89969480870eac01ea69c343add31622e684
susmip <98744813+susmip@users.noreply.github.com>
2022-04-21 15:54:37 +0530

    Update BST.py

--- python/dataStructers/trees/Binary Search Tree/BST.py
+++ python/dataStructers/trees/Binary Search Tree/BST.py
@@ -4,17 +4,60 @@ class BinarySearchTree:
     self.rightChild=None
     self.leftChild=None
 
-def insertNode(rootnode,value):
-    if rootnode.value==None:
-      rootnode.value=value
-    elif rootnode.value>value:
-      if rootnode.leftChild==None:
-        rootnode.leftChild=value
+def insertNode(rootnode,values):
+    if rootnode.value is None:
+      rootnode.value=values
+    elif rootnode.value>=values:
+      if rootnode.leftChild is None:
+        rootnode.leftChild=BinarySearchTree(values)
       else:
-        insertNode(rootnode.leftChild,value)
+        insertNode(rootnode.leftChild,values)
     else:
-      if rootnode.rightChild==None:
-        rootnode.rightChild=value
+      if rootnode.rightChild is None:
+        rootnode.rightChild=BinarySearchTree(values)
       else:
         insertNode(rootnode.rightChild,value)
     return "entered successfully"
+def Preorder(rootnode):
+  if not rootnode:
+     return
+  print(rootnode.value)
+  Preorder(rootnode.leftChild)
+  Preorder(rootnode.rightChild)
+def inorder(rootnode):
+  if not rootnode:
+    return
+  inorder(rootnode.leftChild)
+  print(rootnode.value)
+  inorder(rootnode.rightChild)
+
+def postOrder(rootnode):
+  if not rootnode:
+    return
+  postOrder(rootnode.leftChild)
+  postOrder(rootnode.rightChild)
+  print(rootnode.value)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+BST=BinarySearchTree(None)
+print(insertNode(BST,30))
+print(insertNode(BST,10))
+print(insertNode(BST,40))
+Preorder(BST)
+print()
+inorder(BST)
+print()
+postOrder(BST)
