46a3ef8803b3c252016e0f87aa2216ca1f97d6ef
susmip <98744813+susmip@users.noreply.github.com>
2022-05-10 17:47:51 +0530

    Create CountSort.py

---
+++ python/hackerrank/CountSort.py
@@ -0,0 +1,9 @@
+def countSort(arr):
+    # Write your code here
+    ans=[[] for _ in range(len(arr))]
+    for i,j in enumerate(arr):
+        key,val=j
+        if i<len(arr)//2:
+            val='-'
+        ans[int(key)].append(val)
+    print(( ' '.join([' '.join(i) for i in ans])).strip())
