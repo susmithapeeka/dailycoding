87765c26fd9b102b6764f53e81eca7ac2f2eb411
susmip <98744813+susmip@users.noreply.github.com>
2022-05-10 17:11:54 +0530

    Update binaryHeap.py

--- python/dataStructers/binaryHeap/binaryHeap.py
+++ python/dataStructers/binaryHeap/binaryHeap.py
@@ -45,10 +45,67 @@ def insertNode(rootnode,value,heapType):
   rootnode.heapSize+=1 
   heapifyInsert(rootnode,rootnode.heapSize,heapType)
   return "value inserted"
+
+def deleteEntire(rootnode):
+  rootnode.customList=None
+
+def heapifyTreeExtract(rootNode, index, heapType):
+    leftIndex = index * 2
+    rightIndex = index * 2 + 1
+    swapChild = 0
+
+    if rootNode.heapSize < leftIndex:
+        return
+    elif rootNode.heapSize == leftIndex:
+        if heapType == "Min":
+            if rootNode.customList[index] > rootNode.customList[leftIndex]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[leftIndex]
+                rootNode.customList[leftIndex] = temp
+            return
+        else:
+            if rootNode.customList[index] < rootNode.customList[leftIndex]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[leftIndex]
+                rootNode.customList[leftIndex] = temp
+            return
+
+    else:
+        if heapType == "Min":
+            if rootNode.customList[leftIndex] < rootNode.customList[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.customList[index] > rootNode.customList[swapChild]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[swapChild]
+                rootNode.customList[swapChild] = temp
+        else:
+            if rootNode.customList[leftIndex] > rootNode.customList[rightIndex]:
+                swapChild = leftIndex
+            else:
+                swapChild = rightIndex
+            if rootNode.customList[index] < rootNode.customList[swapChild]:
+                temp = rootNode.customList[index]
+                rootNode.customList[index] = rootNode.customList[swapChild]
+                rootNode.customList[swapChild] = temp
+    heapifyTreeExtract(rootNode, swapChild, heapType)
+
+def extractNode(rootNode, heapType):
+    if rootNode.heapSize == 0:
+        return
+    else:
+        extractedNode = rootNode.customList[1]
+        rootNode.customList[1] = rootNode.customList[rootNode.heapSize]
+        rootNode.customList[rootNode.heapSize] = None
+        rootNode.heapSize -= 1
+        heapifyTreeExtract(rootNode, 1, heapType)
     
 newBinaryItem=heap(5)
 insertNode(newBinaryItem,4,"max")
 insertNode(newBinaryItem,5,"max")
 insertNode(newBinaryItem,2,"max")
 insertNode(newBinaryItem,1,"max")
+print(levelOrder(newBinaryItem))
+extractNode(newBinaryItem,"max")
 print(levelOrder(newBinaryItem))
