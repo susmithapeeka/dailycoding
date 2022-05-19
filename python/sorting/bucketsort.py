7efc3a2cf60ff1fb3926ea2ae50da68663376259
susmip <98744813+susmip@users.noreply.github.com>
2022-05-19 21:59:50 +0530

    Create bucketsort.py

---
+++ python/sorting/bucketsort.py
@@ -0,0 +1,20 @@
+def bucketSort(customList):
+    numberofBuckets = round(math.sqrt(len(customList)))
+    maxValue = max(customList)
+    arr = []
+
+    for i in range(numberofBuckets):
+        arr.append([])
+    for j in customList:
+        index_b = math.ceil(j*numberofBuckets/maxValue)
+        arr[index_b-1].append(j)
+    
+    for i in range(numberofBuckets): 
+        arr[i] = insertionSort(arr[i])
+    
+    k = 0
+    for i in range(numberofBuckets):
+        for j in range(len(arr[i])):
+            customList[k] = arr[i][j]
+            k += 1
+    return customList
