1c753a9e7ffad697152cb44763e7df9a63463878
susmip <98744813+susmip@users.noreply.github.com>
2022-06-11 09:26:47 +0530

    Create Texteditor.py

---
+++ python/hackerrank/Texteditor.py
@@ -0,0 +1,15 @@
+stack = []
+string = ""
+
+for _ in range(int(input())):
+    t = input().split()
+    if t[0]=='1':
+        stack.append(string)
+        string += t[1]
+    elif t[0]=='2':
+        stack.append(string)
+        string = string[:-int(t[1])]
+    elif t[0]=='3':
+        print(string[int(t[1])-1])
+    else:
+        string = stack.pop()
