ed99cc8d2ccbeade008c65f460f7e50ab996fbb7
susmip <98744813+susmip@users.noreply.github.com>
2022-08-06 14:56:10 +0530

    Update quicksort.py

--- python/sorting/quicksort.py
+++ python/sorting/quicksort.py
@@ -12,4 +12,9 @@ def quickSort(customList, low, high):
     if low < high:
         pi = partition(customList, low, high)
         quickSort(customList, low, pi-1)
-        quickSort(customList, pi+1, high) 
+        quickSort(customList, pi+1, high)
+    
+array = [ 10, 7, 8, 9, 1, 5]
+quickSort(array, 0, len(array) - 1)
+  
+print(f'Sorted array: {array}')
